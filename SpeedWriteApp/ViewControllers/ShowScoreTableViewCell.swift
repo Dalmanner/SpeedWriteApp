cc2a097a7b7699e5dcd2a230f0d3455525020275
Dalmanner <145015597+Dalmanner@users.noreply.github.com>
2024-03-28 15:35:30 +0100

    basic start

---
+++ SpeedWriteApp/ViewControllers/ShowScoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShowScoreTableViewCell.swift
+//  SpeedWriteApp
+//
+//  Created by Mac on 2024-03-28.
+//
+
+import UIKit
+
+class ShowScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
