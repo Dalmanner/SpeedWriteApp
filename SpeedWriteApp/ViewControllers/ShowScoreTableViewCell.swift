9a595763f3e37576d3dc4ea6d47deaa3edb97be8
Dalmanner <145015597+Dalmanner@users.noreply.github.com>
2024-04-03 09:45:12 +0200

    removed gitignore

--- SpeedWriteApp/ViewControllers/ShowScoreTableViewCell.swift
+++ SpeedWriteApp/ViewControllers/ShowScoreTableViewCell.swift
@@ -21,14 +21,8 @@ class ShowScoreTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
-        //update the labels for the scores: correctWords, incorrectWords, and wordsPerMinute
-        let correctWordsLabel = UILabel()
-        correctWordsLabel.text = "Correct Words: \(correctWords)"
-        let incorrectWordsLabel = UILabel()
-        incorrectWordsLabel.text = "Incorrect Words: \(incorrectWords)"
-        let wordsPerMinuteLabel = UILabel()
-        wordsPerMinuteLabel.text = "Words Per Minute: \(totalWords / 60)"
-
+        //update the labels for the scores: correctWords, incorrectWords, and wordsPerMinute/totalWords*60
+       
         // Configure the view for the selected state
     }
     
